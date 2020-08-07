20af4c553f753dc4a4ae48b69af0945a19df3ec4
Lindsey Smith <campbelllm@gmail.com>
2020-08-07 16:11:28 -0600

    js time comparison done

--- Develop/script.js
+++ Develop/script.js
@@ -16,42 +16,10 @@ const $timeBlock = $('.time-block');
 
 const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'] 
 
-
-// //checking time of day to change color of inputs
-// console.log(parseInt(newDate.format('HH')), parseInt($hour.attr('data-value')))
-// console.log($hour)
-
-    // if(parseInt(newDate.format('HH')) < parseInt($hour.atrr('value').find())){
-    // $todoText.css('background-color', 'green')
-    // }else if(parseInt(newDate.format('HH')) === parseInt($hour.attr('value'))){
-    // $todoText.css('background-color', 'red')
-    // }else{
-    // $todoText.css('background-color', 'gray').attr('readonly', true)
-    // }  
-    
-    // console.log($hour)
-
-    // $hour.prevObject.each(function(index, singleHour) {
-    //     // console.log("SINGLE HOUR___", singleHour.dataset.value);
-    //     // console.log("CURRENT HOUR___", newDate.format('H'));
-
-    //     console.log(newDate.format('H') < singleHour.dataset.value)
-        // if(newDate.format('H') < singleHour.dataset.value){
-        //     $todoText.css('background-color', 'green')
-        //     }else if(newDate.format('H') === singleHour.dataset.value){
-        //     $todoText.css('background-color', 'red')
-        //     }else{
-        //     $todoText.css('background-color', 'gray').attr('readonly', true)
-        //     }  
-            
-
-    // })
-
     //  LOOP THROUGHT TO CREATE EACH DIV, REMEMBER TO ADD ATRR OF VALUE AND CLASSES
     // ONCE DIVS ARE MADE COMPARE TIME OF EACH DIV, AND GIVE IT A BACKGROUND COLOR ACCORDINGLY
     // SEND VALUE OF TEXT ARE TO LOCAL STORAGE, HAVE THIS BE A PART OF THE LOOP SO IT GRABS THE CORRECT DATA
-    console.log(newDate.format('hA'))
-
+   
     $.each(timeOfDay,function(key,value){
         
         const $hourRow = $('<div>').addClass('row')
@@ -61,16 +29,14 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
         const $btnIcon = $('<i>').addClass('fa fa-save')
         const $submitButton = $('<button>').addClass('saveBtn').append($btnIcon)
         $hourRow.append($hourDiv, $todoText, $submitButton)  
-
-        console.log($hourDiv.text())
-
-        if(newDate.format('hA') < $hourDiv.find().text()){
+        const endTime = moment(value, "hA");
+        if(newDate.isBefore(endTime)){
             $todoText.css('background-color', 'green')
         }else if(newDate.format('hA') === $hourDiv.text()){
-                    $todoText.css('background-color', 'red')}
-        //     }else{
-        //     $todoText.css('background-color', 'gray').attr('readonly', true)
-        //     }  
+                    $todoText.css('background-color', 'red')
+        }else{
+            $todoText.css('background-color', 'gray').attr('readonly', true)
+        }  
     })
 
 
