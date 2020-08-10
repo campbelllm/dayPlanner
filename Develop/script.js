b13c58e37615ca5a7abf8d142ba944b89ae3b241
Lindsey Smith <campbelllm@gmail.com>
2020-08-10 08:33:16 -0600

    updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
-
-
-let $currentDay = $('#currentDay')
-let newDate = moment();
-// const $todoText = $('.todoText')
-// const $hour = $('.hour').find()
-const $container = $('#container')
-// creating and formatting date
-$currentDay.text(newDate.format('dddd, MMMM Do YYYY'))
-
-
-const $timeBlock = $('.time-block');
-
-//array for time of day
-
-const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'] 
-
-    //  LOOP THROUGHT TO CREATE EACH DIV, REMEMBER TO ADD ATRR OF VALUE AND CLASSES
-    // ONCE DIVS ARE MADE COMPARE TIME OF EACH DIV, AND GIVE IT A BACKGROUND COLOR ACCORDINGLY
-    // SEND VALUE OF TEXT ARE TO LOCAL STORAGE, HAVE THIS BE A PART OF THE LOOP SO IT GRABS THE CORRECT DATA
-   
+    let $currentDay = $('#currentDay')
+    let newDate = moment();
+    // const $todoText = $('.todoText')
+    // const $hour = $('.hour').find()
+    const $container = $('#container')
+    // creating and formatting date
+    $currentDay.text(newDate.format('dddd, MMMM Do YYYY'))
+    const $timeBlock = $('.time-block');
+    //array for time of day
+    const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'] 
     $.each(timeOfDay,function(key,value){
         //creating html elements
         const $hourRow = $('<div>').addClass('row')
@@ -30,7 +19,6 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
         const $submitButton = $('<button>').addClass('saveBtn').append($btnIcon)
         //appending created elements to parent container
         $hourRow.append($hourDiv, $todoText, $submitButton)  
-        
         // comparing times in array to current time and applying styling accordingly
         const endTime = moment(value, "hA");
         if(newDate.isBefore(endTime)){
@@ -41,7 +29,6 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
             $todoText.css('background-color', 'gray').attr('readonly', true)
         }  
         //local storage
-
         //setting the text for each time block
         function storeTodoInfo() {
             localStorage.setItem(value, $todoText.val());
@@ -57,56 +44,4 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
         } 
         getTodoText()
     })
-
-    
-
-    function getTodoText(){
-        localStorage.getItem(value,)
-    }
-   //lets create local storage
-
-    // const todoInfo = [];
-    // function storeTodoInfo() {
-    //     localStorage.setItem(value, JSON.stringify($todo.val()));
-    // }
-    
-
-   
-  
-//   //quiz is finished, user inputs name and submits, user name and score pushed to array
-//   scoreButton.addEventListener("click", function (event) {
-//     highScoreList.innerHTML = "";
-//     event.preventDefault();
-//     if(!userName.value){
-//         return false;
-//     };
-//     const userText = userName.value;
-//     userScores.push(userText + " _____ " + "SCORE: " + score);
-//     storeUsers();
-//     renderUsers();
-//     userName.classList.add('hide')
-//     scoreButton.classList.add('hide')
-//     restartButton.classList.remove("hide");
-//   });
-  
-//   // creating li to display userScore array info
-//   function renderUsers() {
-//     for (let i = 0; i < userScores.length; i++) {
-//       const user = JSON.parse(localStorage.getItem("userScores"));
-//       const userList = document.createElement("li");
-//       userList.innerText = user[i];
-//       highScoreList.appendChild(userList);
-//     }
-//   }
-  
-
-
-
-
-
-
-
-
-
-
 })
--- README.md
+++ README.md
@@ -1,46 +1,26 @@
-# 05 Third-Party APIs: Work Day Scheduler
+Homework Week 5 - Day Planner
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Homework Week 4 - Quiz Game
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+Description:
 
-## User Story
+This is a work day planner that changes availability to add to-do's by time of day. 
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+Date changes per day
 
-## Acceptance Criteria
+Timeblocks change color depending on time of day. If the time of day is past, you can't enter any more text. 
 
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-<!-- THEN the current day is displayed at the top of the calendar -->
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+Text is saved to local storage when the save button is clicked. Saved events persist when the page is refreshed.
 
-The following animation demonstrates the application functionality:
+Link to repo:
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
+https://github.com/campbelllm/dayPlanner
 
-## Review
+Link to gitHub pages site:
 
-You are required to submit the following for review:
+https://campbelllm.github.io/dayPlanner/Develop/
 
-* The URL of the deployed application.
+Preview of site:
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+![day planner](./assets/projectScreenShot.png)
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
