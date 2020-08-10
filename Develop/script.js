ca1a4c44ac99123ff5ffac2a0fd04b146c220964
Lindsey Smith <campbelllm@gmail.com>
2020-08-10 08:06:26 -0600

    js dynamic, local storage

--- Develop/script.js
+++ Develop/script.js
@@ -21,14 +21,17 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
     // SEND VALUE OF TEXT ARE TO LOCAL STORAGE, HAVE THIS BE A PART OF THE LOOP SO IT GRABS THE CORRECT DATA
    
     $.each(timeOfDay,function(key,value){
-        
+        //creating html elements
         const $hourRow = $('<div>').addClass('row')
         $timeBlock.append($hourRow)
         const $hourDiv = $('<div>').text(value).addClass('hour') 
-        const $todoText = $('<textArea>').attr('cols', '110').attr('rows', '2')
+        const $todoText = $('<textArea>').attr('cols', '110').attr('rows', '2').attr('value', value)
         const $btnIcon = $('<i>').addClass('fa fa-save')
         const $submitButton = $('<button>').addClass('saveBtn').append($btnIcon)
+        //appending created elements to parent container
         $hourRow.append($hourDiv, $todoText, $submitButton)  
+        
+        // comparing times in array to current time and applying styling accordingly
         const endTime = moment(value, "hA");
         if(newDate.isBefore(endTime)){
             $todoText.css('background-color', 'green')
@@ -37,9 +40,64 @@ const timeOfDay = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
         }else{
             $todoText.css('background-color', 'gray').attr('readonly', true)
         }  
+        //local storage
+
+        //setting the text for each time block
+        function storeTodoInfo() {
+            localStorage.setItem(value, $todoText.val());
+        }
+        //running the function when the save button is clicked
+        $submitButton.on('click', function(){
+            storeTodoInfo()
+         })
+         // retreiving to-do items from local storage and rendering them to their correct time-blocks
+         function getTodoText(){
+            const item = localStorage.getItem(value)
+            $todoText.text(item)
+        } 
+        getTodoText()
     })
 
+    
 
+    function getTodoText(){
+        localStorage.getItem(value,)
+    }
+   //lets create local storage
+
+    // const todoInfo = [];
+    // function storeTodoInfo() {
+    //     localStorage.setItem(value, JSON.stringify($todo.val()));
+    // }
+    
+
+   
+  
+//   //quiz is finished, user inputs name and submits, user name and score pushed to array
+//   scoreButton.addEventListener("click", function (event) {
+//     highScoreList.innerHTML = "";
+//     event.preventDefault();
+//     if(!userName.value){
+//         return false;
+//     };
+//     const userText = userName.value;
+//     userScores.push(userText + " _____ " + "SCORE: " + score);
+//     storeUsers();
+//     renderUsers();
+//     userName.classList.add('hide')
+//     scoreButton.classList.add('hide')
+//     restartButton.classList.remove("hide");
+//   });
+  
+//   // creating li to display userScore array info
+//   function renderUsers() {
+//     for (let i = 0; i < userScores.length; i++) {
+//       const user = JSON.parse(localStorage.getItem("userScores"));
+//       const userList = document.createElement("li");
+//       userList.innerText = user[i];
+//       highScoreList.appendChild(userList);
+//     }
+//   }
   
 
 
